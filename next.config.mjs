76ef702639e4ee41e29d61a1b153fbd6876cf353
elshaddaioheha <105729599+elshaddaioheha@users.noreply.github.com>
2025-10-18 22:37:14 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,23 @@ const nextConfig = {
     'query-string',
     'uint8arrays',
   ],
+  // This adjusts the minifier to prevent the syntax error
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.optimization.minimizer = [
+        ...config.optimization.minimizer,
+      ];
+      config.optimization.minimizer.forEach(minimizer => {
+        if (minimizer.constructor.name === 'TerserPlugin') {
+          minimizer.options.terserOptions.mangle = {
+            ...minimizer.options.terserOptions.mangle,
+            safari10: true, // Prevents a class of errors related to variable name reuse
+          };
+        }
+      });
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
