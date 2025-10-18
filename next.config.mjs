748833fc27fe2ef10dbb6d149f7297ca1957f852
elshaddaioheha <105729599+elshaddaioheha@users.noreply.github.com>
2025-10-18 23:15:19 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,6 @@
-import { createRequire } from 'node:module';
-const require = createRequire(import.meta.url);
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  swcMinify: true,
-  transpilePackages: [
-    '@hashgraph/sdk',
-    'hashconnect',
-    '@hashgraph/cryptography',
-    '@hashgraph/proto',
-  ],
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      'long': require.resolve('long'),
-      'protobufjs': require.resolve('protobufjs/minimal'),
-    };
-    return config;
-  },
+  transpilePackages: ['@hashgraph/sdk', 'hashconnect'],
 };
 
 export default nextConfig;
